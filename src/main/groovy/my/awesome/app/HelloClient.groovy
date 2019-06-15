8078f7f794993f9b80c77859ff43ca9d9bd6d198
Andrey Vetlugin <antrew@gmail.com>
2019-06-15 13:50:02 +0200

    mn create-client HelloClient

---
+++ src/main/groovy/my/awesome/app/HelloClient.groovy
@@ -0,0 +1,12 @@
+package my.awesome.app
+
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.HttpStatus
+
+@Client("hello")
+interface HelloClient {
+
+    @Get("/")
+    HttpStatus index()
+}
